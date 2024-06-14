d503993e79f1e16b5ba79f2ce1da3719d5a6821a
CoderDrew02 <andrew.eaton02@gmail.com>
2024-06-14 11:45:03 -0400

    fixed life cycle issue, added vertical win condition (working), added negative slope diagonal win condition (working), added positive slope win condition (not working)

--- client/connect-four/src/components/GameBoard.vue
+++ client/connect-four/src/components/GameBoard.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="header">
         <h3>Connect Four</h3>
-        <h4  v-bind:style="{ color: currentPlayer === 'red' ? 'red' : 'yellow' }">It's {{ currentPlayer }}'s turn!</h4>
+        <h4 v-if="!gameOver" v-bind:style="{ color: currentPlayer === 'red' ? 'red' : 'yellow' }">It's {{ currentPlayer }}'s turn!</h4>
         <h4 v-if="gameOver">Game Over! {{ winner }} wins!</h4>
     </div>
 
@@ -76,11 +76,11 @@ export default {
     data() {
         return {
             currentPlayer: "red", // red goes first
-            movesPlayed: 0, // no moves have been played yet
             rowPosition: ["1", "2", "3", "4", "5", "6"],
             columnPosition: ["a", "b", "c", "d", "e", "f", "g"],
             gameOver: false,
             winner: "",
+            elementId: null,
 
         }
     },
@@ -90,15 +90,19 @@ export default {
         playChip(event) // function to play a chip
         {
 
-            const elementId = event.target; // get the element that was clicked
+            const cell = event.target; // get the element that was clicked
+            this.elementId = cell.id.split("");
 
-            if (!elementId.classList.contains("red") && !elementId.classList.contains("yellow")) // if the element does not have a color, cannot overwrite other play's chip
+            //row = elementId[1]
+            //column = elementId[0]
+
+
+            if (!this.gameOver && !cell.classList.contains("red") && !cell.classList.contains("yellow") && cell.classList.contains("row")) // if the element does not have a color, cannot overwrite other play's chip
             {
-                elementId.classList.add(this.currentPlayer); // add the current player's color to the element
-                this.movesPlayed++; // increment the number of moves played
-                this.currentPlayer = this.currentPlayer === "red" ? "yellow" : "red"; // switch players
+                cell.classList.add(this.currentPlayer); // add the current player's color to the element
                 this.checkWinCon(); // check for win conditions
-               
+                this.currentPlayer = this.currentPlayer === "red" ? "yellow" : "red"; // switch players
+                
             }
 
             //need to create a function to check for win conditions
@@ -108,58 +112,86 @@ export default {
 
 
         },
+        //check for win conditions
         checkWinCon() {
-            //check for win conditions
-            // if (this.movesPlayed >= 7) {
-                //check for horizontal win
-                    for (let i = 0; i < this.rowPosition.length; i++) {
-                        for (let j = 0; j < this.columnPosition.length; j++) {
-                            if (document.getElementById(this.columnPosition[j] + this.rowPosition[i]).classList.contains(this.currentPlayer) &&
-                                document.getElementById(this.columnPosition[j + 1] + this.rowPosition[i]).classList.contains(this.currentPlayer) &&
-                                document.getElementById(this.columnPosition[j + 2] + this.rowPosition[i]).classList.contains(this.currentPlayer) &&
-                                document.getElementById(this.columnPosition[j + 3] + this.rowPosition[i]).classList.contains(this.currentPlayer)) {
-                                if (this.currentPlayer == "red") {
-                                    this.gameOver = true;
-                                    this.winner = "Red";
-                                    
-                                } else {
-                                    this.gameOver = true;
-                                    this.winner = "Yellow";
-                                    
-                            }
-                    }
+            //check for horizontal win
+            //searches only for win condition in the row (elementId[1]) that was last played --> saves computing power & memory
+            for (let j = 0; j < this.columnPosition.length - 3; j++) {
+                
+                if (document.getElementById(this.columnPosition[j] +     this.elementId[1]).classList.contains(this.currentPlayer) &&
+                    document.getElementById(this.columnPosition[j + 1] + this.elementId[1]).classList.contains(this.currentPlayer) &&
+                    document.getElementById(this.columnPosition[j + 2] + this.elementId[1]).classList.contains(this.currentPlayer) &&
+                    document.getElementById(this.columnPosition[j + 3] + this.elementId[1]).classList.contains(this.currentPlayer)) 
+                {
+                    
+                    this.gameOver = true;
+                    this.winner = this.currentPlayer;
+
                 }
             }
+    
             //check for vertical win
-            for (let j = 0; j < this.columnPosition.length; j++) {
-                        for (let i = 0; i < this.rowPosition.length; i++) {
-                            if (document.getElementById(this.columnPosition[j] + this.rowPosition[i]).classList.contains(this.currentPlayer) &&
-                                document.getElementById(this.columnPosition[j] + this.rowPosition[i + 1]).classList.contains(this.currentPlayer) &&
-                                document.getElementById(this.columnPosition[j] + this.rowPosition[i + 2]).classList.contains(this.currentPlayer) &&
-                                document.getElementById(this.columnPosition[j] + this.rowPosition[i + 3]).classList.contains(this.currentPlayer)) {
-                                if (this.currentPlayer == "red") {
-                                    this.gameOver = true;
-                                    this.winner = "Red";
-                                    
-                                } else {
-                                    this.gameOver = true;
-                                    this.winner = "Yellow";
-                                    
-                            }
+            //searches only for win condition in the column (elementId[0]) that was last played --> saves computing power & memory
+            for (let i = 0; i < this.rowPosition.length - 3; i++) {
+
+                if (document.getElementById(this.elementId[0] + this.rowPosition[i]).classList.contains(this.currentPlayer) &&
+                    document.getElementById(this.elementId[0] + this.rowPosition[i + 1]).classList.contains(this.currentPlayer) &&
+                    document.getElementById(this.elementId[0] + this.rowPosition[i + 2]).classList.contains(this.currentPlayer) &&
+                    document.getElementById(this.elementId[0] + this.rowPosition[i + 3]).classList.contains(this.currentPlayer)) 
+                    {
+                    
+                    this.gameOver = true;
+                    this.winner = this.currentPlayer;
+                
+                    }
+            }
+                
+            //check for diagonal win (negative slope) - rows increase, columns decrease
+            for (let i = 0; i < this.rowPosition.length - 3; i++) {
+                for(let j = 0; j < this.columnPosition.length - 3; j++) {
+                    
+                    if (
+                    document.getElementById(this.columnPosition[j] + this.rowPosition[i]).classList.contains(this.currentPlayer) &&
+                    document.getElementById(this.columnPosition[j + 1] + this.rowPosition[i + 1]).classList.contains(this.currentPlayer) &&
+                    document.getElementById(this.columnPosition[j + 2] + this.rowPosition[i + 2]).classList.contains(this.currentPlayer) &&
+                    document.getElementById(this.columnPosition[j + 3] + this.rowPosition[i + 3]).classList.contains(this.currentPlayer)
+                    ) 
+                    
+                    {
+                    
+                    this.gameOver = true;
+                    this.winner = this.currentPlayer;
+
                     }
                 }
-            // }
-                
-                
-                //check for diagonal win
-                
-            
-        }
-        },
+            }    
 
-    }
+            //check for diagonal win (positive slope) - rows decrease, columns increase
+            for (let i = this.rowPosition.length; i > 3; i--) {
+                for(let j = 0; j < this.columnPosition.length - 3; j++) {
+                    
+                    if (
+                    document.getElementById(this.columnPosition[j] + this.rowPosition[i]).classList.contains(this.currentPlayer) &&
+                    document.getElementById(this.columnPosition[j + 1] + this.rowPosition[i - 1]).classList.contains(this.currentPlayer) &&
+                    document.getElementById(this.columnPosition[j + 2] + this.rowPosition[i - 2]).classList.contains(this.currentPlayer) &&
+                    document.getElementById(this.columnPosition[j + 3] + this.rowPosition[i - 3]).classList.contains(this.currentPlayer)
+                    ) 
+                    
+                    {
+                    
+                    this.gameOver = true;
+                    this.winner = this.currentPlayer;
 
-}
+                    }
+                }
+            }    
+
+
+
+
+        } // end check win condition
+    } // end methods
+} // end export / default
 
 
 </script>
